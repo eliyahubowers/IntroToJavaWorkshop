f6f3426705a3f1efa32b1192ec88a4b56dfe9dce
JackChine27 <league@WTS14.attlocal.net>
2017-09-24 11:29:48 -0700

    day 4d

--- src/day4/DragonFight.java
+++ src/day4/DragonFight.java
@@ -1,22 +1,25 @@
 package day4;
 
+import javax.swing.JOptionPane;
+
 public class DragonFight {
 	// 1. Create a main() method and put all of the code below inside of it
-	
+	public static void main(String[] args) {
 	// 2. Pop up a message telling the user: "You found a dragon!  Defeat it to take its treasure"
-	
+	JOptionPane.showMessageDialog(null, "you found a dragon defeat it to take its gold");
 	// 3. Create a variable to store the user's health (set it equal to 100)
-	
+	int uhealth = 100;
 	// 4. Create a variable to store the dragon's health (set it to 100)
-	
+	int dhealth = 100;
 	// 5. Ask the if they want to attack the dragon with a yell or a kick
-	
+	String attack = JOptionPane.showInputDialog("do you want to attack with a yell or a kick?");
 	// 6. If they typed in "yell":
-	
+	if(attack.equals("yell")) {
 		//-- Find a random number between 0 and 10 
-	
+	new Random().nextInt(11);
 		//-- Subtract that number from the dragon's health variable 
-	
+		
+	}
 	// 7. If they typed in "kick":
 		
 		//-- Find a random number between 5 and 25
@@ -36,5 +39,5 @@ public class DragonFight {
 	// 12. If the dragon's health is less than or equal to 0
 	
 		//-- Tell the user that the dragon is dead and they took a ton of gold!
-		
+	}
 }
